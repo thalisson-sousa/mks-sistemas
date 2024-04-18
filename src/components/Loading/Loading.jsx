9125a43d9b22f21c4255ef238c59287462d11e00
Thalisson <thalissonsousa33@gmail.com>
2024-04-18 00:22:12 -0300

    feat: Loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {FaTruckLoading} from 'react-icons/fa';
+
+import './Loading.css';
+
+export default function Loading() {
+  return (<FaTruckLoading className="loading"/>);
+}
