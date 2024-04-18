63f3657c44090ef5bf9363ac36452dc5fc2ef3b4
Thalisson <thalissonsousa33@gmail.com>
2024-04-18 00:23:01 -0300

    feat: Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="Container">
+      <p>MKS sistemas © Todos os direitos reservados</p>
+    </footer>
+  );
+}
